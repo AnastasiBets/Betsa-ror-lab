a7d8bd104091f160af06c87b07aad5faa19b600f
ruby -v <anastasia.betsa@gmail.com>
2017-10-21 12:30:58 +0300

    registration user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
 	validates :lastname, :email, :password,  uniqueness: true
 	#validates :password, numericality: true
 	validates :password, numericality: { only_integer: true }
+#include PrettyEmailValidatable
+#validates :pretty_email, email: true
+	#validates :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+	#validates :email, email_format: { message: "doesn't look like an email address" }
 end
