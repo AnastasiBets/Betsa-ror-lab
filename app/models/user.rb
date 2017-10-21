357df3ce53011d531c7c076b0245ed3c99273751
ruby -v <anastasia.betsa@gmail.com>
2017-10-21 12:15:54 +0300

    registration user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
 	validates :firstname, :lastname, :email, :password, presence: true
 	validates :lastname, :email, :password,  uniqueness: true
+	#validates :password, numericality: true
+	validates :password, numericality: { only_integer: true }
 end
