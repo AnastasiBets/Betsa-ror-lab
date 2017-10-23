8c201ddcc8f4ebf0ab552d0fdd815d7dde27760a
ruby -v <anastasia.betsa@gmail.com>
2017-10-23 10:52:02 +0300

    email validation, ES(th) letters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
 	validates :firstname, :lastname, :email, :password,:country, :city, :house_number, presence: true
 	validates :house_number, numericality: { only_integer: true  }	
+	validates :firstname, :lastname, :country, :city, format: { with: /\A[a-zA-Z]+\z/, message: ":only letters!"}
+	validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "has invalid email adress"}
 	#validates :firstname, :lastname, :email, :password, presence: true
 	#validates :lastname, :email, :password,  uniqueness: true
 	#validates :password, numericality: true
